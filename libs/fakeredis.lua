4abfa425787bfcf124759588eeab5e1bdfa16b2b
hiword1396 <akh110@gmail.com>
2017-03-28 15:02:19 +0430

    Update fakeredis.lua

--- libs/fakeredis.lua
+++ libs/fakeredis.lua
@@ -1707,7 +1707,7 @@ return {
   new = new,
 }
 -- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- http://hiword.ir
+-- @hiword
+-- @hiword
+-- @hiword
